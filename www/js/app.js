3c8834dbbbfef320c47271db5cca5077581b973e
李蘭國 <13726268701@163.com>
2016-02-04 17:18:41 +0800

    README.md

---
+++ www/js/app.js
@@ -0,0 +1,109 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.tabs', {
+    url: "/tabs",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/tabs.html"
+      }
+    }
+  })
+
+  .state('app.tabs.tab1', {
+    url: '/tab1',
+    views: {
+      'tab-tab1': {
+        templateUrl: 'templates/tab-tab1.html'
+      }
+    }
+  })
+
+  .state('app.tabs.tab2', {
+    url: '/tab2',
+    views: {
+      'tab-tab2': {
+        templateUrl: 'templates/tab-tab2.html'
+      }
+    }
+  })
+
+  .state('app.tabs.tab3', {
+    url: '/tab3',
+    views: {
+      'tab-tab3': {
+        templateUrl: 'templates/tab-tab3.html'
+      }
+    }
+  })
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
+      }
+    }
+  })
+
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/tabs');
+});
